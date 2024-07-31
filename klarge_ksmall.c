5861a5671d79dd8811e893073698f4c134df816c
Praveen473 <101171452+Praveen473@users.noreply.github.com>
2024-07-31 20:43:16 +0530

    Add files via upload

---
+++ klarge_ksmall.c
@@ -0,0 +1,56 @@
+//kth largest kth smallest
+#include<stdio.h>
+int main()
+{
+    int s,l;
+    int small,large,a[100],n,ans1,ans2,ksmall,klarge;
+    printf("ENter N");
+    scanf("%d",&n);
+    printf("ENter kmall");
+    scanf("%d",&ksmall);
+    printf("ENter klarge");
+    scanf("%d",&klarge);
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(int i=0;i<n;i++)
+    {
+        s=1;
+        l=1;
+        for(int j=0;j<n;j++)
+        {
+            ans1=a[i];
+            ans2=a[i];
+            if(a[i]>a[j])
+            {
+                printf("small a[i] %d\n",a[i]);
+                s=s+1;
+                printf("small s avlue%d\n",s);
+                ans1=a[i];
+            }
+            if(a[i]<a[j])
+            {
+                printf("large a[i] %d\n",a[i]);
+                l=l+1;
+                printf("large l value %d\n",l);
+                ans2=a[i];
+            }
+        }
+        printf("outside s%d\n",s);
+        printf("outside l%d\n",l);
+        printf("outside ksmall %d\n",ksmall);
+        if(ksmall==s)
+        {
+            printf("inside if s value %d\n",s);
+            printf("answer1 %d\n",ans1);
+        }
+        printf("outside klarge %d\n",klarge);
+        if(klarge==l)
+        {
+            printf("inside if l value %d\n",l);
+            printf("answer2 %d\n",ans2);
+        }
+    }
+
+}
